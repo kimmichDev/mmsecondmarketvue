c9b0decfffb998173ea28bdee16f4ab7e5890f08
kimmichDev <myueway98@gmail.com>
2022-07-29 03:08:33 +0630

    finish register

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import { useCheckOnline } from "./composables/useCheckOnline";
 export default {
   setup() {
     console.log(useStore().state.auth);
+    console.log("version" + process.env.VUE_APP_VERSION);
     useCheckOnline();
     if (process.env.VUE_APP_VERSION !== "1") {
       useRouter().push({ name: "test" });
